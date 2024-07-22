bc2adeb5564e568d891dae3073c8693d312996a2
epodak <epodak@gmail.com>
2024-07-22 23:17:10 +0800

    :sparkles: Feat 更新一个版本

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -6,6 +6,22 @@ document.getElementById('openLinksButton').addEventListener('click', function ()
     });
 });
 
+document.getElementById('openPanButton').addEventListener('click', function () {
+    let pattern = '^http://www\\.zhuzhupan2\\.com/gotopan_pay';
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { message: "openLinks", pattern: pattern });
+    });
+});
+
+document.getElementById('openUkButton').addEventListener('click', function () {
+    let pattern = '^http://www\\.zhuzhupan2\\.com/uk\\?id=';
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { message: "openLinks", pattern: pattern });
+    });
+});
+
 document.getElementById('optionsButton').addEventListener('click', function () {
     chrome.runtime.openOptionsPage();
 });
